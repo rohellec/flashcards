89fc990da6784d0e8631709e6ba6a99d6f8840c0
Dima Rogov <ds.rogv@gmail.com>
2017-02-18 19:54:16 +0300

    Add validations to Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate  :can_not_be_equal
+
+  private
+
+    def can_not_be_equal
+      if !(original_text.blank? || translated_text.blank?) &&
+           translated_text.strip.casecmp?(original_text.strip)
+        errors.add(:translated_text, "can't be equal to original text")
+      end
+    end
 end
