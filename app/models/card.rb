f797cea2ade9eb1f0a998a1eb42d7ae6294a9ec2
Dima Rogov <ds.rogv@gmail.com>
2017-02-25 12:35:08 +0300

    Add callback to set card's review date on create

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,8 @@ class Card < ApplicationRecord
   validates :original_text, :translated_text, :review_date, presence: true
   validate  :can_not_be_equal
 
+  before_validation :set_original_review_date, on: :create
+
   private
 
     def can_not_be_equal
@@ -9,5 +11,9 @@ class Card < ApplicationRecord
            translated_text.strip.casecmp?(original_text.strip)
         errors.add(:translated_text, "can't be equal to original text")
       end
+    end
+
+    def set_original_review_date
+      self.review_date = Date.today.advance(days: 3)
     end
 end
