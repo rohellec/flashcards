e2ff49129aca590094209684e33506503ddd194c
Dima Rogov <ds.rogv@gmail.com>
2017-08-07 23:53:21 +0300

    Improve cards review process

--- app/models/card.rb
+++ app/models/card.rb
@@ -15,31 +15,55 @@ class Card < ApplicationRecord
 
   before_validation :set_next_review_date, on: :create
 
+  REVIEW_DATES = {
+    0 => Time.current,
+    1 => 12.hours.from_now,
+    2 => 3.days.from_now,
+    3 => 1.week.from_now,
+    4 => 2.weeks.from_now,
+    5 => 1.month.from_now
+  }.freeze
+
   def picture_remote_url=(url_value)
     return if url_value.blank?
     self.picture = URI.parse(url_value) unless picture.file?
     @picture_remote_url = url_value
   end
 
-  def right_translation?(param)
-    param.strip.casecmp?(original_text)
-  end
-
-  def update_next_review_date
+  def review(param)
+    result = param.strip.casecmp?(original_text)
+    if result
+      self.fail_reviews = 0
+      increase_success_reviews
+    else
+      self.fail_reviews += 1
+      decrease_success_reviews
+    end
     set_next_review_date
     save
+    result
   end
 
   private
 
-    def can_not_be_equal
-      if !(original_text.blank? || translated_text.blank?) &&
-           translated_text.strip.casecmp?(original_text.strip)
-        errors.add(:translated_text, "can't be equal to original text")
-      end
+  def can_not_be_equal
+    if !(original_text.blank? || translated_text.blank?) &&
+         translated_text.strip.casecmp?(original_text.strip)
+      errors.add(:translated_text, "can't be equal to original text")
     end
+  end
 
-    def set_next_review_date
-      self.review_date = 3.days.from_now
-    end
+  def decrease_success_reviews
+    return if success_reviews.zero? || !(fail_reviews % 3).zero?
+    self.success_reviews -= 1
+  end
+
+  def increase_success_reviews
+    return if success_reviews >= REVIEW_DATES.keys.max
+    self.success_reviews += 1
+  end
+
+  def set_next_review_date
+    self.review_date = REVIEW_DATES[success_reviews]
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -13,9 +13,98 @@ describe Card do
     end
   end
 
-  describe ":review_date after creation" do
-    it "is set to 3 days from now" do
-      expect(card.review_date).to eq(3.days.from_now.to_date)
+  describe "#review(arg)" do
+    let(:review_tries) do
+      {
+        1 => 12.hours.from_now.to_date,
+        2 => 3.days.from_now.to_date,
+        3 => 1.week.from_now.to_date,
+        4 => 2.weeks.from_now.to_date,
+        5 => 1.month.from_now.to_date
+      }
+    end
+    let(:right) { card.original_text }
+    let(:wrong) { card.original_text * 2 }
+
+    it "returns true if arg == :original_text" do
+      expect(card.review(right)).to be_truthy
+    end
+
+    it "returns false if arg != :original_text" do
+      expect(card.review(wrong)).to be_falsey
+    end
+
+    describe ":review_date" do
+      context "after creation" do
+        it "is set to current date" do
+          expect(card.review_date).to eq(Date.current)
+        end
+      end
+
+      context "after successful reviews" do
+        it "is set according to a number of review tries" do
+          review_tries.each do |num, date|
+            card.review(card.original_text)
+            expect(card.review_date).to eq(date)
+          end
+        end
+      end
+
+      context "after 3 failed reviews in a row" do
+        context "before any successful review" do
+          it "is set to current date" do
+            3.times { card.review(wrong) }
+            expect(card.review_date).to eq(Date.current)
+          end
+        end
+
+        context "if there was a number of successful reviews before" do
+          before do
+            5.times { card.review(card.original_text) }
+          end
+
+          it "is set according to a previous number of review tries" do
+            (1..4).reverse_each do |num|
+              3.times { card.review(wrong) }
+              expect(card.review_date).to eq(review_tries[num])
+            end
+          end
+        end
+      end
+    end
+
+    describe ":success_reviews" do
+      it "is increased after successful review" do
+        expect { card.review(card.original_text) }.to change(card, :success_reviews).by(1)
+      end
+
+      it "is decreased after 3 failed reviews in a row" do
+        card.success_reviews = 1
+        3.times { card.review(wrong) }
+        expect(card.success_reviews).to be_zero
+      end
+
+      it "is not decreased after less than 3 failed reviews in a row" do
+        card.success_reviews = 1
+        2.times { card.review(wrong) }
+        expect(card.success_reviews).to eq(1)
+      end
+
+      it "can't be less than zero" do
+        card.success_reviews = 0
+        expect { card.review(wrong) }.not_to change(card, :success_reviews)
+      end
+    end
+
+    describe ":fail_reviews" do
+      it "is increased after failed review" do
+        expect { card.review(wrong) }.to change(card, :fail_reviews).by(1)
+      end
+
+      it "is set to 0 after successful review" do
+        card.review(card.original_text)
+        expect(card.fail_reviews).to be_zero
+      end
     end
   end
 
@@ -28,18 +117,6 @@ describe Card do
     it "doesn't contain cards with review_date later then today" do
       card.update(review_date: Date.tomorrow)
       expect(Card.for_review).not_to include(card)
-    end
-  end
-
-  describe "#right_translation?(arg)" do
-    it "returns true if arg == :original_text" do
-      arg = card.original_text
-      expect(card.right_translation?(arg)).to be_truthy
-    end
-
-    it "returns false if arg != :original_text" do
-      arg = card.original_text * 2
-      expect(card.right_translation?(arg)).to be_falsey
     end
   end
 end
